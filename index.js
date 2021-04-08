087c7c67e256e1985f95c88c3be4097675d4f7f0
Alicia Kmak <alicia.kmak@gmail.com>
2021-04-08 18:36:18 +0200

    add iterations 1-5

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    return this.items.sort((a, b) => a.length - b.length);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.indexOf(pos)== -1) {
+      throw new Error("OutOfBounds");
+    }
+    else {
+      return this.items.indexOf(pos)
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+      if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
   sum() {}
 
